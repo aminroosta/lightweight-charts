f10ab83df8ee6874eed510ea4bb95585afad3b22
amin roosta <amin.roosta@outlook.com>
2020-09-05 11:36:47 +0800

    fix date

--- src/formatters/format-date.ts
+++ src/formatters/format-date.ts
@@ -1,15 +1,11 @@
-import { numberToStringWithLeadingZero as numToStr, toFarsiNumber } from './price-formatter';
+const getMonth = (date: Date, locale: string) => date.toLocaleDateString(locale).split('/')[1];
+const getDay = (date: Date, locale: string) => date.toLocaleDateString(locale).split('/')[2];
 
-const getMonth = (date: Date) => date.getUTCMonth() + 1;
-const getDay = (date: Date) => date.getUTCDate();
-/* const getYear = (date: Date) => date.getUTCFullYear(); */
-
-const dd = (date: Date) => toFarsiNumber(numToStr(getDay(date), 2));
+const dd = (date: Date, locale: string) => getDay(date, locale);
 const MMMM = (date: Date, locale: string) => new Date(date.getUTCFullYear(), date.getUTCMonth(), 1)
 	.toLocaleString(locale, { month: 'long' });
-const MMM = (date: Date, locale: string) => new Date(date.getUTCFullYear(), date.getUTCMonth(), 1)
-	.toLocaleString(locale, { month: 'short' });
-const MM = (date: Date) => toFarsiNumber(numToStr(getMonth(date), 2));
+const MMM = (date: Date, locale: string) => date.toLocaleString(locale, { month: 'short' });
+const MM = (date: Date, locale: string) => getMonth(date, locale);
 const yy = (date: Date, locale: string) => date.toLocaleDateString(locale).split('/')[0].slice(2,4);
 const yyyy = (date: Date, locale: string) => date.toLocaleDateString(locale).split('/')[0];
 
@@ -19,7 +15,7 @@ export function formatDate(date: Date, format: string, locale: string): string {
 		.replace(/yy/g, yy(date, locale))
 		.replace(/MMMM/g, MMMM(date, locale))
 		.replace(/MMM/g, MMM(date, locale))
-		.replace(/MM/g, MM(date))
-		.replace(/dd/g, dd(date))
+		.replace(/MM/g, MM(date, locale))
+		.replace(/dd/g, dd(date, locale))
 	;
 }
